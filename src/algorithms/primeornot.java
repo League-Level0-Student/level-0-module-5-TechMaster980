9ab5a81b13e33e382898f4c2bcb1a516cf2e1279
league <league@imac-6.attlocal.net>
2019-03-02 12:59:06 -0800

    finish one code only here.

---
+++ src/algorithms/primeornot.java
@@ -0,0 +1,37 @@
+package algorithms;
+
+import javax.swing.JOptionPane;
+
+public class primeornot {
+	
+public static void main(String[] args) {
+
+String prime = JOptionPane.showInputDialog("Put a number in");
+
+int p = Integer.parseInt(prime);
+boolean isPrime = true;
+
+if (p%2==0) {
+	isPrime=false;
+}
+else {
+	for(int i = 3; i<p; i+=2) {
+		if(p % i == 0) {
+			isPrime=false;
+			break;
+	    }
+
+     }
+}
+
+if(isPrime) {
+	System.out.println("prime");
+}
+else {
+	System.out.println("not prime");
+}
+
+}
+
+
+}
