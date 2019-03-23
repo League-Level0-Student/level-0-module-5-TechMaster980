60ba2552b385550bf9911427614d93ce739e6955
CesarTadeo <league@imac-6.attlocal.net>
2019-03-23 12:53:29 -0700

    they were easy and fun to do.

---
+++ src/Forloopgaunlet.java
@@ -0,0 +1,59 @@
+
+public class Forloopgaunlet {
+public static void main(String[] args) {
+	
+///----number 1-------------
+
+//for(int i=0; i<=100; i++) {
+//	System.out.println(i);
+//}
+
+
+///----number 2-------------
+
+//for(int i=100; i>=0; i--) {
+//System.out.println(i);
+//}
+
+
+///----number 3-------------
+
+//for(int i=2; i<=100; i+=2) {
+//System.out.println(i);
+//}
+	
+	
+///----number 4-------------
+
+//for(int i=1; i<=99; i+=2) {
+//System.out.println(i);
+//}
+
+	///----number 5-------------	
+	
+	//for(int i =0; i<=500; i++) {
+	//System.out.println(i);
+	//if(i%2==0) {
+	//System.out.println("even");	
+	//}
+	//else {
+	//System.out.println("odd");	
+	//}
+	//}
+
+	
+	///----number 6-------------
+	
+	//for(int i =0; i<=777; i+=7) {
+	//System.out.println(i);	
+	//}
+	
+	
+	///----number 7-------------
+	
+	//for(int i =0; i<=11; i++) {
+	//System.out.println("In "+(2008+i)+", i was "+i+" years old.");	
+	//}
+	
+}
+}
