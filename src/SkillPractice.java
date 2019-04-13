88f71992a786cf8ca81c6e5f1b211ab54a9ead51
NikitaD111 <league@192.168.7.175>
2019-04-13 12:52:53 -0700

    it was kind of hard to do and easy at the end

--- src/SkillPractice.java
+++ src/SkillPractice.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
@@ -5,11 +7,11 @@ public class SkillPractice {
 public static void main(String[] args) {
 	
 	SkillPractice skills = new SkillPractice();
-  	skills.skill1();
+  	//skills.skill1();
   	//skills.skill2();
   	//skills.skill3();
   	//skills.skill4();
-  	//skills.skill5();
+  	skills.skill5();
 }
 
 void skill1() {
@@ -34,5 +36,56 @@ void skill1() {
 	}
 }
 
+void skill2() { 
+for(int i=2; i < 30; i+=3) {
+System.out.println(i);
 }
+}
+void skill3() { 
+Random rd=new Random();
+int result=rd.nextInt(20);
+System.out.println(result);
+int result1=rd.nextInt(10);
+System.out.println(result1);
+JOptionPane.showMessageDialog(null, result-result1);
+}
+
+void skill4() { 
+
+String city = JOptionPane.showInputDialog("what city do you live at");
+if(city.equals("sandiego")) {
+JOptionPane.showMessageDialog(null, "you live in america's finest city");
+}
+else {
+JOptionPane.showMessageDialog(null, "you should move to san diego");	
+}
+int cars	 = 2;
+if(cars==0) {
+JOptionPane.showMessageDialog(null, "I bet you use public transportation.");	
+}
+if(cars==1) {
+JOptionPane.showMessageDialog(null, "the model of your car");	
+}
+if(cars>1) {
+JOptionPane.showMessageDialog(null, "how many wheels does the car have between them");	
+}
+}
+
+void skill5() { 
+String school = JOptionPane.showInputDialog("what the name of your school");
+JOptionPane.showMessageDialog(null, (school)+" is a great school");
+
+
+
+	}
+	}
+
+
+
+
+
+
+
+
+
 
